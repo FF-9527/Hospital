d3998621894da65b9409b13fad0df618eb48d71c
dongQa <2553301512@qq.com>
2019-12-11 18:14:25 +0800

    测试

---
+++ src/main/java/com/qf/hospital/patient/controller/PatientController.java
@@ -0,0 +1,11 @@
+package com.qf.hospital.patient.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class PatientController {
+
+    public void select() {
+        System.out.println("asdfghjkl");
+    }
+}
