6f95b9fb55265521b573deb4aa2c784227f1ed5e
dongQa <2553301512@qq.com>
2019-12-11 17:44:18 +0800

    xiugai

--- src/main/java/com/qf/hospital/patient/controller/PatientController.java
+++ src/main/java/com/qf/hospital/patient/controller/PatientController.java
@@ -2,9 +2,6 @@ package com.qf.hospital.patient.controller;
 
 import org.springframework.stereotype.Controller;
 
-import java.util.ArrayList;
-import java.util.List;
-
 @Controller
 public class PatientController {
 
